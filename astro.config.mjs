10e4c74f0edee77b64d40a1c7b707084de2a4c60
francisco hernan <Franciscogjjgjgjh@gmail.com>
2026-01-03 17:38:59 -0700

    cuarto333

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/server';
 
 
  
@@ -7,5 +8,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
    output: 'server',
+  adapter: vercel(),
 
 });
